a5664bd1b0a1bc653e66c3fb50b9107e721a2b7f
shongeorg <shongeorg48@gmail.com>
2024-11-09 12:07:26 +0200

    project

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
 const express = require("express");
-const { Client } = require("pg");
 const app = express();
-require("dotenv").config();
+const port = 3000;
 
-const port = process.env.PORT || 3000;
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+app.get("/", (req, res) => {
+  res.send("Hello, Docker!");
 });
 
-const connectWithRetry = () => {
-  client
-    .connect()
-    .then(() => console.log("Connected to PostgreSQL"))
-    .catch((err) => {
-      console.error("Connection error", err.stack);
-      setTimeout(connectWithRetry, 5000);
-    });
-};
-
-connectWithRetry();
-
-app.get("/", async (req, res) => {
-  try {
-    const result = await client.query("SELECT NOW()");
-    res.send(`Connected to PostgreSQL at: ${result.rows[0].now}`);
-  } catch (err) {
-    console.error("Query error", err.stack);
-    res.status(500).send("Error querying the database");
-  }
-});
-
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server is running on port 3000");
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
